42d29975bb640d8517bf3c51b29e63d9dec5c7c3
Shahzeb Afroze <shahzebkhan197@gmail.com>
2018-08-17 19:33:56 +0500

    UI and new features

--- add_site.py
+++ add_site.py
@@ -14,56 +14,72 @@ An algorithim to add your favourite sites to the list.
 import sys
 import pyperclip
 
-
-# extracted content from your clipboard
-clipboard = pyperclip.paste().strip()
-
-
 # extracting the websitelist to show the user.
-website_list = 'websites.txt'
-r = open(website_list,'r')
-content = r.read()
-websites_list = content.strip().split('\n')
-
-# opening file to make additions
-f = open(website_list,'a')
+file = 'websites.txt'
+with open(file,'r') as r:
+	time = r.readline(1)
+	content = r.read()
+	websites_list = content.strip().split('\n')
+	del_empty_strings = [website for website in websites_list if website != ""]
 
 # UI 
 print "\n->The current list of websites:\n "
-for count,website in enumerate(websites_list,1):
+
+for count,website in enumerate(del_empty_strings,1):
 	print str(count) +')', website
+
+def update_list(final_list):
+
+	print("\n->The updated website list in {}:\n").format(file)
+	
+	f = open(file,'w')
+	n = len(final_list[count-1])
+
+	if n>40:
+		n=30
+
+	for i,site in enumerate(final_list,1):
+
+		if i == count+1:
+			print "updates".center(n+4,'-')
+			print ""
+
+		print str(i) + ") " + site
+		f.write(site + '\n\n')
+
+	f.close()
+	print ""
+	
 
 
 # Does the user want to add from clipboard or directly?
 
 if len(sys.argv)>1:
 
-	# command line content held by the variable - raw
+	# command line content held by the variable - returned in list data type
 	raw = sys.argv[1:]
 
+	# convert the "string" with args to split elements 
 	for el in raw:
 		content = el.split(',')
 
-	print("->The following link/links has been added to {}:\n").format(website_list)
+	final_list = del_empty_strings + content 
+	update_list(final_list)
 	
-	for i,site in enumerate(content,count+1):
-		
-		f.write(site +'\n')
-		print str(i) + ") " + site
-
-	print ""
-
 
 else:
+
+	clipboard = str(pyperclip.paste().strip())
 
 	print "\n->Would you like to add the following link to the file?\n"
 	print str(count + 1) + ") " + clipboard
 
 	user_input = raw_input("\n->Press enter or return to add the content in your clipboard.")
 
-	if user_input == "":
-		print "->The link has been successfully added to {}\n".format(website_list)
-		f.write(clipboard + '\n')
+	if user_input == "" :
+		final_list = del_empty_strings[:]
+		final_list.append(clipboard)
+		update_list(final_list)
 
 
 
